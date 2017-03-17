a8d2d6ce3420a3a90da397aa705e187dced0d341
LiuLongChang <1040719208@qq.com>
2017-03-17 14:04:16 +0800

    submit project

---
+++ ScrollViewHoverDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewHoverDemo
+//
+//  Created by zzzsw on 2017/3/17.
+//  Copyright © 2017年 sunbohong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
